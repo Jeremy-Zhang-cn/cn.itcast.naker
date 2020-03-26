c0d389293c7f4bf47df1820ec32f2f6007323da6
JeremyZhang <1159387269@qq.com>
2020-03-26 19:07:00 +0800

    springboot_day02 actuator&&admin-server; 2020_03_26

--- framework_adv_section_01/springboot-listener/src/main/java/com/itheima/springbootlistener/runner/MySpringApplicationRunListener.java
+++ framework_adv_section_01/springboot-listener/src/main/java/com/itheima/springbootlistener/runner/MySpringApplicationRunListener.java
@@ -1,10 +1,14 @@
 package com.itheima.springbootlistener.runner;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 public class MySpringApplicationRunListener implements SpringApplicationRunListener {
+
+	public MySpringApplicationRunListener(SpringApplication application, String[] args) {
+	}
 
 	@Override
 	public void starting() {
